018872c7f4bf71b752c2be6d6fc2170dc05b724d
Michał Kruszewski <mkru@protonmail.com>
2020-07-23 10:30:25 +0200

    Reorganize vhdl_lang binary into subcommands.
    By the way fix the fact, that number of threads parameter
    did not work becaseu of lack of 'takes_value()'.

--- vhdl_lang/src/main.rs
+++ vhdl_lang/src/main.rs
@@ -14,37 +14,44 @@ extern crate clap;
 
 use std::path::Path;
 use std::time::SystemTime;
-use vhdl_lang::{Config, Diagnostic, MessagePrinter, Project};
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+
+use vhdl_lang::{Config, MessagePrinter, Project};
 
 fn main() {
-    use clap::{App, Arg};
-
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(
-            Arg::with_name("num-threads")
-                .short("-p")
-                .long("--num-threads")
-                .help("The number of threads to use. By default the maximum is selected based on process cores"),
-        )
-        .arg(
-            Arg::with_name("perf")
-                .long("--perf")
-                .help("Prints the number of files processed and the execution time")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("config")
-                .help("Config file in TOML format containing libraries and settings")
-                .short("-c")
-                .long("--config")
-                .required(true)
-                .takes_value(true),
+        .subcommand(
+            SubCommand::with_name("perf")
+                .about("Analyse project, print diagnostics and performance")
+                .arg(
+                    Arg::with_name("num-threads")
+                        .short("-t")
+                        .long("--threads")
+                        .help("The number of threads to use. By default the maximum is selected based on process cores")
+                        .takes_value(true)
+                )
+                .arg(
+                    Arg::with_name("config")
+                        .help("Config file in TOML format containing libraries and settings")
+                        .short("-c")
+                        .long("--config")
+                        .required(true)
+                        .takes_value(true),
+                )
         )
         .get_matches();
 
+    match matches.subcommand_name() {
+        Some("perf") => perf(matches.subcommand_matches("perf").unwrap()),
+        _ => {}
+    }
+}
+
+fn perf(matches: &ArgMatches) {
     if matches.is_present("num-threads") {
         let num_threads = value_t_or_exit!(matches.value_of("num-threads"), usize);
         rayon::ThreadPoolBuilder::new()
@@ -52,8 +59,6 @@ fn main() {
             .build_global()
             .unwrap();
     }
-
-    let show_perf = matches.is_present("perf");
 
     let file_name = value_t_or_exit!(matches.value_of("config"), String);
     let mut config = Config::default();
@@ -68,36 +73,31 @@ fn main() {
     let mut project = Project::from_config(&config, &mut msg_printer);
     let diagnostics = project.analyse();
     let duration = start.elapsed().unwrap();
-    show_diagnostics(&diagnostics);
 
-    if show_perf {
-        let mut num_files = 0;
-        let mut num_lines = 0;
-        for source_file in project.files() {
-            num_files += 1;
-            num_lines += source_file.num_lines();
-        }
-        let duration_per_file = duration.checked_div(num_files as u32).unwrap();
-        let duration_per_line = duration.checked_div(num_lines as u32).unwrap();
-
-        println!(
-            "Analyzed {} files with {} lines of code",
-            num_files, num_lines
-        );
-        println!(
-            "Total time to run was {} ms with an average of {} us per file and an average {} ns per line",
-            duration.as_millis(),
-            duration_per_file.as_micros(),
-            duration_per_line.as_nanos()
-        );
+    for d in diagnostics {
+        println!("{}", d.show());
     }
+
+    let mut num_files = 0;
+    let mut num_lines = 0;
+    for source_file in project.files() {
+        num_files += 1;
+        num_lines += source_file.num_lines();
+    }
+    let duration_per_file = duration.checked_div(num_files as u32).unwrap();
+    let duration_per_line = duration.checked_div(num_lines as u32).unwrap();
+
+    println!(
+        "Analyzed {} files with {} lines of code",
+        num_files, num_lines
+    );
+    println!(
+        "Total time to run was {} ms with an average of {} us per file and an average {} ns per line",
+        duration.as_millis(),
+        duration_per_file.as_micros(),
+        duration_per_line.as_nanos()
+    );
 
     // Exit without running Drop on entire allocated AST
     std::process::exit(0);
-}
-
-fn show_diagnostics(diagnostics: &[Diagnostic]) {
-    for diagnostic in diagnostics {
-        println!("{}", diagnostic.show());
-    }
 }
